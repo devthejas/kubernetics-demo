fd23917f30580c124945f91ca5689463791aade1
Thejas K S <thejasks04@gmail.com>
2026-04-23 12:15:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,41 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
-}
-}
+
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
+
+# Generate a random suffix for the bucket name
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
+
+# S3 bucket with unique name
 resource "aws_s3_bucket" "demo_bucket" {
-bucket = "jenkins-terraform-demo-12345"
+  bucket = "jenkins-terraform-demo-${random_integer.suffix.result}"
 }
-resource "aws_vpc" "demo_vpc" {
-cidr_block = "10.0.0.0/16"
+
+# Use the default VPC instead of creating a new one
+data "aws_vpc" "default" {
+  default = true
 }
+
+# Create a subnet inside the default VPC
 resource "aws_subnet" "public_subnet" {
-vpc_id = aws_vpc.demo_vpc.id
-cidr_block = "10.0.1.0/24"
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = "10.0.1.0/24"
 }
+
+# Launch an EC2 instance in the subnet
 resource "aws_instance" "ec2" {
-ami = "ami-0f5ee92e2d63afc18"
-instance_type = "t2.micro"
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
 }
